a7a56ed13971af135fe2fda1cbb1af8697a69abb
rafaelhnastos <rafaelhenribastos12@gmail.com>
2021-05-20 14:37:11 -0300

    anime-numbers became class

--- js/modules/anime-numbers.js
+++ js/modules/anime-numbers.js
@@ -1,34 +1,46 @@
-export default function initAnimeNumbers() {
+export default class AnimeNumbers {
+  constructor(numbers, observerTarget, observerClass) {
+    this.numbers = document.querySelectorAll(numbers);
+    this.observerTarget = document.querySelector(observerTarget);;
+    this.observerClass = observerClass;
 
-  function animeNumbers() {
-    const numbers = document.querySelectorAll('[data-number]');
-    
-    numbers.forEach(number => {
-      const total = +number.innerText; 
-      const increment = Math.floor(total/100);
-
-      let start = 0;
-      const timer = setInterval(() => {
-        start+=increment;
-        number.innerText = start;
-        if(start > total){
-          number.innerText = total;
-          clearInterval(timer);
-        }
-      }, 50 * Math.random);
-    });
-  
+    this.handleMutation = this.handleMutation.bind(this)
   }
-  let observer;
 
-  function handleMutation(mutation) {
-    if(mutation[0].target.classList.contains('active')){
-      observer.disconnect();
-      animeNumbers();
+  static incrementNumber(number) {
+    const total = +number.innerText;
+    const increment = Math.floor(total / 100);
+    let start = 0;
+    const timer = setInterval(() => {
+      start += increment;
+      number.innerText = start;
+      if (start > total) {
+        number.innerText = total;
+        clearInterval(timer);
+      }
+    }, 50 * Math.random());
+  }
+
+  animeNumbers() {
+    this.numbers.forEach(number => this.constructor.incrementNumber(number));
+  }
+
+  handleMutation(mutation) {
+    if (mutation[0].target.classList.contains(this.observerClass)) {
+      this.observer.disconnect();
+      this.animeNumbers();
     }
   }
 
-  observer = new MutationObserver(handleMutation);
-  const observerTarget = document.querySelector('.numbers');
-  observer.observe(observerTarget, {attributes: true});
+  addMutationObserver() {
+    this.observer = new MutationObserver(this.handleMutation);
+    this.observer.observe(this.observerTarget, { attributes: true });
+  }
+
+  init() {
+    if (this.numbers.length && this.observerTarget) {
+      this.addMutationObserver();
+    }
+    return this;
+  }
 }
--- js/modules/fetch-animals.js
+++ js/modules/fetch-animals.js
@@ -1,16 +1,16 @@
-import initAnimeNumbers from './anime-numbers.js';
+import AnimeNumbers from './anime-numbers.js';
 
 export default function initFetchAnimals() {
-  function createAnimal(animal){
+  function createAnimal(animal) {
     const div = document.createElement('div');
     div.classList.add('number-animal');
 
     div.innerHTML = `<h3>${animal.specie}</h3> <span data-number>${animal.total}</span>`;
     return div;
   }
-  
-  async function fetchAnimals(url){
-    try{
+
+  async function fetchAnimals(url) {
+    try {
       const animalsResponse = await fetch(url);
       const animalsJSON = await animalsResponse.json();
       const numbersGrid = document.querySelector('.numbers-grid');
@@ -19,8 +19,9 @@ export default function initFetchAnimals() {
         const divAnimal = createAnimal(animal);
         numbersGrid.appendChild(divAnimal);
       });
-      initAnimeNumbers();
-    } catch(erro){
+      const animeNumbers = new AnimeNumbers('[data-number]', '.numbers', 'active');
+      animeNumbers.init();
+    } catch (erro) {
       console.log(erro);
     }
   }
